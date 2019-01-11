bb4417ff7d0dae224f7a10b7e8754690f8e584dc
Alberto <alberto@avidad.com>
2019-01-11 02:30:42 +0100

    Modificado el script de compilar

--- compila.sh
+++ compila.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
+chmod 755 grx-dbus/DEBIAN/*
 RUTA=$(pwd)
 cd $RUTA/system.d/
+qmake
 make clean
 make
 cp grx.dbus $RUTA/grx-dbus/usr/sbin/grx.dbus.service
 cd $RUTA/session.d/
+qmake
 make clean
 make
 cp grx.dbus $RUTA/grx-dbus/usr/bin/grx.dbus.service
